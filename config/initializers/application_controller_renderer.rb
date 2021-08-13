99dfb56608980cbeb68ef8b5faeace71074f639c
makaroniletus1783 <daoyeyoushu@gmail.com>
2021-08-13 15:01:42 +0000

    [Update]Railsを学ぼう

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '624c5b70b352d224d9ddaa146e8ea6b5ec3bc0c391ec5fe13a08cfaff4f0640fae1c9bd469a4a1fe84b0b6fc90db9e12c04790485d8d805bc6b54806c5ba13b0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_035752) do
+ActiveRecord::Schema.define(version: 2021_08_13_135300) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
